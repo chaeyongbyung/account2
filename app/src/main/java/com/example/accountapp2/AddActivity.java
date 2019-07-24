9f0dd77941fec71c6cff99c2e81faff9b3c20f05
ChaeHyeonU <wwwa0528@gmail.com>
2019-07-24 18:05:25 +0900

    수정중

--- app/src/main/java/com/example/accountapp2/AddActivity.java
+++ app/src/main/java/com/example/accountapp2/AddActivity.java
@@ -1,34 +1,128 @@
 package com.example.accountapp2;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.DatePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddActivity extends AppCompatActivity {
+
+    //현재시간을 msec으로 구함
+    long now = System.currentTimeMillis();
+    //현재시간을 date변수에 저장
+    Date date = new Date(now);
+    //시간을 나타낼 포맷 정하기
+    SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy년 MM월 dd일");
+    //nowDate 변수에 값 저장
+    String formatDate = sdfNow.format(date);
+
+    private TextView input_date;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+
+    //플래그값  1=수익  2=지출
+    int flag = 2;
+
+    class BtnOnClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Button in_money_btn = (Button) findViewById(R.id.in_money_btn);
+            Button out_money_btn = (Button) findViewById(R.id.out_money_btn);
+            Button move_money_btn = (Button) findViewById(R.id.move_money_btn);
+            switch (view.getId()) {
+                case R.id.in_money_btn:
+                    flag = 1;
+                    in_money_btn.setBackgroundResource(R.color.darkbrown);
+                    out_money_btn.setBackgroundResource(R.color.white);
+                    move_money_btn.setBackgroundResource(R.color.white);
+                    break;
+                case R.id.out_money_btn:
+                    flag = 2;
+                    in_money_btn.setBackgroundResource(R.color.white);
+                    out_money_btn.setBackgroundResource(R.color.darkbrown);
+                    move_money_btn.setBackgroundResource(R.color.white);
+                    break;
+                case R.id.move_money_btn:
+                    flag = 3;
+                    in_money_btn.setBackgroundResource(R.color.white);
+                    out_money_btn.setBackgroundResource(R.color.white);
+                    move_money_btn.setBackgroundResource(R.color.darkbrown);
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_page);
 
+        input_date = (TextView)findViewById(R.id.input_date);
+        input_date.setText(formatDate);
+
+        //달력에서 값받기
+        this.InitializeView();
+        this.InitializeListener();
+
+        BtnOnClickListener onClickListener = new BtnOnClickListener() ;
+
+        // 각 Button의 이벤트 리스너로 onClickListener 지정.
+        Button in_money_btn = (Button) findViewById(R.id.in_money_btn) ;
+        in_money_btn.setOnClickListener(onClickListener) ;
+        Button out_money_btn = (Button) findViewById(R.id.out_money_btn) ;
+        out_money_btn.setOnClickListener(onClickListener) ;
+        Button move_money_btn = (Button) findViewById(R.id.move_money_btn) ;
+        move_money_btn.setOnClickListener(onClickListener) ;
+
+
     }
+
     public void mOnClick(View view) {
-        Intent intent =null;
+        Intent intent = null;
+
         switch(view.getId()){
             case R.id.backbtn2:
                 intent =new Intent(AddActivity.this, MainActivity.class);
                 break;
-            case R.id.in_money_btn:
-                intent =new Intent(AddActivity.this, AddIncomeActivity.class);
-                break;
-            case R.id.out_money_btn:
-                intent =new Intent(AddActivity.this, AddActivity.class);
-                break;
+
         }
         startActivity(intent);
     }
+
+
+    //달력에서 값 받기용
+    public void InitializeView()
+    {
+        input_date = (TextView)findViewById(R.id.input_date);
+    }
+
+    public void InitializeListener()
+    {
+        callbackMethod = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                input_date.setText(year +"년 "+ month + "월 " + day + "일");
+            }
+        };
+    }
+
+    public void OnClickHandler(View view)
+    {
+        DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 7, 24);
+
+        dialog.show();
+    }
+
+
+
 }
--- app/src/main/java/com/example/accountapp2/MainActivity.java
+++ app/src/main/java/com/example/accountapp2/MainActivity.java
@@ -1,33 +1,104 @@
 package com.example.accountapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.DatePicker;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView this_month;
+    int mYear;
+    int mMonth;
+    int mDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+         this_month = (TextView) findViewById(R.id.this_month);
+
+        Calendar cal = new GregorianCalendar();
+        mYear = cal.get(Calendar.YEAR);
+        mMonth = cal.get(Calendar.MONTH);
+        mDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        BtnOnClickListener onClickListener = new BtnOnClickListener() ;
+        // 각 Button의 이벤트 리스너로 onClickListener 지정.
+        ImageView before_month = (ImageView) findViewById(R.id.before_month) ;
+        before_month.setOnClickListener(onClickListener) ;
+        ImageView after_month = (ImageView) findViewById(R.id.after_month) ;
+        after_month.setOnClickListener(onClickListener) ;
+
+        UpdateNow();
+
     }
+
     public void mOnClick(View view) {
-        Intent intent =null;
+        Intent intent = null;
         switch(view.getId()){
+            case R.id.this_month:
+                new DatePickerDialog(MainActivity.this, mDateSetListener, mYear, mMonth, mDay).show();
+                break;
             case R.id.btn_add:
                 intent =new Intent(MainActivity.this, AddActivity.class);
                 break;
             case R.id.btn_list:
                 intent =new Intent(MainActivity.this, ListActivity.class);
                 break;
-            case R.id.before_month:
-                intent =new Intent(MainActivity.this, AddActivity.class);
-                break;
         }
         startActivity(intent);
     }
+
+    DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+            // TODO Auto-generated method stub
+
+            mYear = year;
+
+            mMonth = monthOfYear;
+
+            mDay = dayOfMonth;
+
+            UpdateNow();
+        }
+    };
+
+    class BtnOnClickListener implements Button.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            TextView this_month = (TextView) findViewById(R.id.this_month);
+            switch (view.getId()) {
+                case R.id.before_month:
+
+                    break;
+                case R.id.after_month:
+
+                    break;
+            }
+        }
+    }
+
+    void UpdateNow(){
+
+        this_month.setText(String.format("%d.%d.%d", mYear, mMonth + 1 , mDay));
+
+    }
+
 }
